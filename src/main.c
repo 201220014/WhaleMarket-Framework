331b61d4dca7f6fd8fc287cf7ff9e80264f19613
JiaCai Cui <201220014@smail.nju.edu.cn>
2022-01-13 13:27:59 +0800

    Modify

--- src/main.c
+++ src/main.c
@@ -1,11 +1,7 @@
 #include "tools/hint.h"
 
-#include "test/hello.h"
-
 int main() {
     welcomeMessage();
-
-    hello();
     invalidMessage();
     return 0;
 }
--- src/test/hello.c
+++
@@ -1,6 +0,0 @@
-#include "test/hello.h"
-#include <stdio.h>
-
-void hello() {
-    printf("hello\n");
-}
